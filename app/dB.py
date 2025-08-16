0deb5b8ff1b03d921c6992715c6119fb39f8aec0
AlinCqe <alinemanuelv@gmail.com>
2025-08-17 00:20:59 +0300

    WIP: Migration to PostgreSQL #1

--- app/dB.py
+++ app/dB.py
@@ -1,5 +1,35 @@
 import sqlite3
 
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, func, and_, any_
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+Base = declarative_base()
+
+class Sheet(Base):
+    __tablename__ = 'sheets'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    safe_filename = Column(String, nullable=False)
+    song_name = Column(String, nullable=False)
+    authors = Column(ARRAY(String))
+    categories = Column(ARRAY(String))
+    instruments = Column(ARRAY(String))
+    uploaed_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
+
+Base.metadata.create_all(bind=engine)
+
+SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
+
+
 
 def create_tables():
     with sqlite3.connect('note_nest.db') as conn:
@@ -17,34 +47,52 @@ def create_tables():
             )
         """)    
 
-def get_sheets_from_dB(song_name,author,category,instrument):
+
+
+def get_sheets_from_dB(song_name,authors,categories,instruments):
 
     filters = []
-    params = []
+
     if song_name:
-        filters.append('song_name = ?')
-        params.append(song_name)
+        filters.append(Sheet.song_name == song_name)
 
-    if author:
-        filters.append('author = ?')
-        params.append(author)
+    if authors:
+        if isinstance(authors, list):
+            filters.append(Sheet.authors.contains(authors))
+        else:    
+            filters.append(authors == any_(Sheet.authors))
 
-    if category:
-        filters.append('category = ?')
-        params.append(category)
+    if categories:
+        if isinstance(categories, list):
+            filters.append(Sheet.categories.contains(categories))
+        else:
+            filters.append(instruments == any_(Sheet.categories))
 
-    if instrument:
-        filters.append('instrument = ?')
-        params.append(instrument)
+    if instruments:
+        if isinstance(instruments, list):
+            filters.append(Sheet.instruments.contains(instruments))
+        else:
+            filters.append(instruments == any_(Sheet.instruments))
 
-    where_clause = ' AND '.join(filters)
-    query = 'SELECT * FROM sheets'
 
-    if where_clause:
-        query += ' WHERE ' + where_clause
-    
-    print(query)
+    with SessionLocal() as session:
+        if filters:
+            data = session.query(Sheet).filter(and_(*filters)).all()
+        else:
+            data =  data = session.query(Sheet).all()
 
+        sheets = [    {
+        "id": sheet.id,
+        "song_name": sheet.song_name,
+        "authors": sheet.authors,
+        "categories": sheet.categories,
+        "instruments": sheet.instruments
+        }
+        for sheet in data
+
+    ]
+        print(sheets)
+        return sheets
 
     with sqlite3.connect('note_nest.db') as conn:
         conn.row_factory = sqlite3.Row
@@ -54,19 +102,34 @@ def get_sheets_from_dB(song_name,author,category,instrument):
         rows = cursor.fetchall()        
         data = [dict(row) for row in rows]
         
-        return data
+        
     
-def insert_sheet(safe_filename, song_name, author, category, instrument):
+
+def insert_sheet(safe_filename, song_name, authors, categories, instruments):
+
     with sqlite3.connect('note_nest.db') as conn:
         cursor = conn.cursor()
 
-        cursor.execute("INSERT INTO sheets (safe_filename,song_name, author, category, instrument) VALUES (?,?,?,?,?)", (safe_filename,song_name,  author, category, instrument))
-
+        cursor.execute("INSERT INTO sheets (safe_filename,song_name, author, category, instrument) VALUES (?,?,?,?,?)", (safe_filename,song_name,  authors, categories, instruments))
         conn.commit()
+
+    with SessionLocal() as session:
+        session.add(Sheet(safe_filename=safe_filename,song_name=song_name,authors=[authors],categories=[categories],instruments=[instruments]))
+        session.commit()
+
 
 
 #works, return right safe filename
 def get_safe_file_name(song_name):
+
+    with SessionLocal() as session:
+        safe_filename = session.query(Sheet.safe_filename).filter(Sheet.song_name == song_name).scalar()
+        return safe_filename
+
+
+
+
+
     with sqlite3.connect('note_nest.db') as conn:
 
         cursor = conn.cursor()
@@ -74,5 +137,4 @@ def get_safe_file_name(song_name):
         cursor.execute('SELECT safe_filename FROM sheets WHERE song_name = ?', (song_name,))
 
         result  = cursor.fetchone()
-        return result[0] if result else None
 
--- app/routes.py
+++ app/routes.py
@@ -17,9 +17,9 @@ def upload_file():
     file = request.files['file']
     safe_filename = f'{uuid.uuid4()}.{file.filename}'
     song_name = request.form.get('song_name', '')
-    author = request.form.get('author', '')
-    category = request.form.get('category', '')
-    instrument = request.form.get('instrument', '')
+    authors = request.form.get('author', '')
+    categories = request.form.get('category', '')
+    instruments = request.form.get('instrument', '')
 
 
     os.makedirs('app/static/uploads',exist_ok=True)
@@ -30,7 +30,7 @@ def upload_file():
     file.save(os.path.join('app/static/uploads', safe_filename))
     
 
-    insert_sheet(safe_filename=safe_filename, song_name=song_name, author=author, category=category, instrument=instrument)
+    insert_sheet(safe_filename=safe_filename, song_name=song_name, authors=authors, categories=categories, instruments=instruments)
     
     return jsonify('x')
 
@@ -47,11 +47,19 @@ def download(song_name):
 @routes.route('/get_sheets', methods=['GET'])
 def get_sheets():
 
-    song_name = request.args.get('song_name', '')
-    author = request.args.get('author', '')
-    category = request.args.get('category', '')
-    instrument = request.args.get('instrument', '')
+    song_name = request.args.get('song_name', None)
 
+    authors = request.args.get('author', None)
+    if authors:
+        authors = authors.split(',')
 
-    return get_sheets_from_dB(song_name,author,category,instrument)
+    categories = request.args.get('category', None)
+    if categories:
+        categories = categories.split(',')
+
+    instruments = request.args.get('instrument', None)
+    if instruments:
+        instruments = instruments.split(',')
+
+    return get_sheets_from_dB(song_name,authors,categories,instruments)
 
